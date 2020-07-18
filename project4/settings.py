0db4c63966dd8e802f265073a80711f96ead21a3
poojacse93 <rvps19@gmail.com>
2020-07-18 22:49:16 +0530

    message

--- project4/settings.py
+++ project4/settings.py
@@ -78,6 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE' : 'django.db.backends.postgressql',
+       # 'NAME': 'postgres',
+       # 'USER': 'postgres',
+       # 'PORT': 5432       
     }
 }
 
