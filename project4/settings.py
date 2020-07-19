66e9f6b82eadaeab9a99582958c8f7112ad9a298
poojacse93 <rvps19@gmail.com>
2020-07-19 15:37:24 +0530

    message

--- project4/settings.py
+++ project4/settings.py
@@ -127,14 +127,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+
 
 
--- project4/wsgi.py
+++ project4/wsgi.py
@@ -16,3 +16,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project4.settings')
 
 application = get_wsgi_application()
 
+
+
