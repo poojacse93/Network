9558b027b61707b2d1610ede23e452b378488ada
poojacse93 <rvps19@gmail.com>
2020-09-01 16:08:28 +0530

    done

--- project4/settings.py
+++ project4/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'Write Your Key here')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 #DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
